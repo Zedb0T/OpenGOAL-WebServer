ff1774f1568ccd559cf398ec17a478328f568bea
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2023-03-13 16:12:53 -0400

    test

--- pyserver.py
+++ pyserver.py
@@ -15,7 +15,7 @@ def get_public_ip():
 
 
 pathToDataTxt = "DICT.txt"
-ip_address = get_public_ip()
+ip_address = "78.108.218.126"
 server_address = (ip_address, 25560)
 
 # Initialize the dictionary from the file
